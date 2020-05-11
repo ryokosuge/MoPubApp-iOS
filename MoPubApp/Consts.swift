529857dc3082e67b771a52b90256176a9dc65b3a
ryokosuge <ryo.kosuge@gmail.com>
2020-05-11 18:40:57 +0900

    feat:	バナーとレクタングルを実装

--- MoPubApp/Consts.swift
+++ MoPubApp/Consts.swift
@@ -13,5 +13,7 @@ struct Consts {
         static let initialize: String = "0e62da92382645149878800bf5aac967"
         static let rewardedVideo: String = "0e62da92382645149878800bf5aac967"
         static let fullscreen: String = "dfff8665442742f7b8a3b16269e78f9f"
+        static let banner: String = "1451632c23764ae68e2ffb98d4835bf5"
+        static let rectangle: String = "c59a040851c24e1f86034a78fa195b57"
     }
 }
---
+++ MoPubApp/ViewControllers/Banner/RectangleViewController.swift
@@ -0,0 +1,83 @@
+//
+//  RectangleViewController.swift
+//  MoPubApp
+//
+//  Created by ryokosuge on 2020/05/11.
+//  Copyright © 2020 ryokosuge. All rights reserved.
+//
+
+import UIKit
+import MoPub
+
+class RectangleViewController: UIViewController {
+    
+    private var adView: MPAdView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadAd()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        adView?.center = view.center
+    }
+    
+}
+
+extension RectangleViewController {
+    
+    @IBAction func refresh(_ barButtonItem: UIBarButtonItem) {
+        adView?.loadAd(withMaxAdSize: kMPPresetMaxAdSize250Height)
+    }
+    
+    private func loadAd() {
+        adView?.removeFromSuperview()
+        adView = nil
+        
+        guard let adView = MPAdView(adUnitId: Consts.AdUnitID.rectangle) else {
+            return
+        }
+        
+        adView.delegate = self
+        adView.frame = CGRect(x: 0, y: 0, width: 300, height: 250)
+        view.addSubview(adView)
+        adView.loadAd(withMaxAdSize: kMPPresetMaxAdSize250Height)
+        
+        self.adView = adView
+    }
+    
+}
+
+extension RectangleViewController: MPAdViewDelegate {
+    
+    func viewControllerForPresentingModalView() -> UIViewController! {
+        return self
+    }
+    
+    func adViewDidLoadAd(_ view: MPAdView!, adSize: CGSize) {
+        print(#function, view!, view!.adUnitId ?? "nil", adSize)
+    }
+    
+    func adView(_ view: MPAdView!, didFailToLoadAdWithError error: Error!) {
+        print(#function, view!, view!.adUnitId ?? "nil", error.localizedDescription)
+    }
+    
+    func didDismissModalView(forAd view: MPAdView!) {
+        print(#function, view!, view!.adUnitId ?? "nil")
+    }
+    
+    func willPresentModalView(forAd view: MPAdView!) {
+        print(#function, view!, view!.adUnitId ?? "nil")
+    }
+    
+    func willLeaveApplication(fromAd view: MPAdView!) {
+        print(#function, view!, view!.adUnitId ?? "nil")
+    }
+    
+}
